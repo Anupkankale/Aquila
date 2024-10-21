78f5d6371ee23e0d9be5519c118c679b16040017
anupkankale <anupkankaleak47@gmail.com>
2024-10-21 15:34:37 +0530

    By using filemtime( get_template_directory().'/style.css'));

--- functions.php
+++ functions.php
@@ -4,3 +4,19 @@
  * 
  * @package Aquila
  */
+
+//To Check Style Sheet PAth WAs Working Or Not  
+//   echo '<pre>';
+//     print_r(get_template_directory());
+//     wp_die();
+
+ function aquila_enqueue_script(){
+
+    //wp_enqueue_style('stylesheet',get_template_directory_uri().'/style.css');
+    wp_enqueue_style('stylesheett',get_stylesheet_uri(), [], filemtime( get_template_directory().'/style.css'));
+
+ }
+
+ add_action('wp_enqueue_scripts','aquila_enqueue_script');
+
+ ?>
