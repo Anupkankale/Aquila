f573fb8df25f103db37e6495390c645c1f0d03d5
anupkankale <anupkankaleak47@gmail.com>
2024-10-21 17:57:19 +0530

    Add The Bootstrap

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Theme Function 
  * 
@@ -10,14 +11,24 @@
 //     print_r(get_template_directory());
 //     wp_die();
 
- function aquila_enqueue_script(){
+function aquila_enqueue_script()
+{
 
-    //wp_enqueue_style('stylesheet',get_template_directory_uri().'/style.css');
-    wp_enqueue_style('styles-heett',get_stylesheet_uri(), [], filemtime( get_template_directory().'/style.css'));
-    wp_enqueue_script('main-js',get_template_directory_uri(). '/assets/main.js', [], filemtime( get_template_directory().'/assets/main.js' ), true);
+    // Register Style.
+    wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'));
+    wp_register_style('bootstrap-css', get_template_directory_uri() .'/assets/src/library/node_modules/bootstrap/dist/css/bootstrap.css', [],false,'all');
 
- }
+    // Register Script
+    wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
+    wp_register_script('bootstrap-js', get_template_directory_uri() .'/assets/src/library/node_modules/bootstrap/dist/js/bootstrap.min.js', [ 'jquery' ],false,true); 
 
- add_action('wp_enqueue_scripts','aquila_enqueue_script');
+    //Enqueue Styles
+    wp_enqueue_style('style-css');
+    wp_enqueue_style('bootstrap-css');
 
- ?>
+    //Enqueue Script
+    wp_enqueue_script('main-js');
+    wp_enqueue_script('bootstrap-js');
+}
+
+add_action('wp_enqueue_scripts', 'aquila_enqueue_script');
--- header.php
+++ header.php
@@ -13,9 +13,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body <?php body_class('hello-class'); ?>>
+<body <?php body_class( ); ?>>
 
-<?php wp_body_open(); ?>
+<?php
+ if(function_exists('wp_body_open')){
+
+    wp_body_open();
+
+ }
+ ?>
     
 
  <header> Header </header>
