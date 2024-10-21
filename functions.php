f37fa9e77c5ea3bd59a06b007d2daa2c26a605ba
anupkankale <anupkankaleak47@gmail.com>
2024-10-21 15:52:29 +0530

    Add Thr JavaScript File in the Footer Section enqueue_script

--- functions.php
+++ functions.php
@@ -13,7 +13,8 @@
  function aquila_enqueue_script(){
 
     //wp_enqueue_style('stylesheet',get_template_directory_uri().'/style.css');
-    wp_enqueue_style('stylesheett',get_stylesheet_uri(), [], filemtime( get_template_directory().'/style.css'));
+    wp_enqueue_style('styles-heett',get_stylesheet_uri(), [], filemtime( get_template_directory().'/style.css'));
+    wp_enqueue_script('main-js',get_template_directory_uri(). '/assets/main.js', [], filemtime( get_template_directory().'/assets/main.js' ), true);
 
  }
 
