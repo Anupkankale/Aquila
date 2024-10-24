75911778ec16280f8f28f2d8b1ad41917d77e714
anupkankale <anupkankaleak47@gmail.com>
2024-10-24 12:31:47 +0530

    For Site Logo and Custom logo Added

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -34,6 +34,16 @@ class AQUILA_THEME
 
         //Its Is Important For Custmization option in Them To Add Site Title
         public function setup_theme(){
-          add_theme_support('title-tag');  
+          add_theme_support('title-tag'); 
+
+          //These Section For Site Logo
+          add_theme_support('custom-logo', [
+            'header-text' => ['site-title', 'site-description'],
+            'height'=> 100,
+		    'width' => 400,
+		    'flex-height' => true,
+		    'flex-width'  => true,
+	        'unlink-homepage-logo' => true, 
+        ]); 
         }
 }
--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -9,6 +9,11 @@
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <?php 
+         if(function_exists('the_custom_logo')){
+          the_custom_logo();
+         }
+  ?>
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -33,13 +38,8 @@
           <a class="dropdown-item" href="#">Something else here</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
+      
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    
   </div>
 </nav>
