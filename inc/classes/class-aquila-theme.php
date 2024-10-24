90262440ec86e095735bef82724af710b5e3962e
anupkankale <anupkankaleak47@gmail.com>
2024-10-24 17:39:03 +0530

    Theme Support Has Added And Update  in the Code

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -24,26 +24,60 @@ class AQUILA_THEME
         $this->setup_hooks();
     }
 
-    protected function setup_hooks() {
+    protected function setup_hooks()
+    {
         /**
          * Action
          */
-        add_action('after_setup_theme',[ $this,'setup_theme']);
+        add_action('after_setup_theme', [$this, 'setup_theme']);
+    }
 
-        }
+    //Its Is Important For Custmization option in Them To Add Site Title
+    public function setup_theme()
+    {
+        add_theme_support('title-tag');
 
-        //Its Is Important For Custmization option in Them To Add Site Title
-        public function setup_theme(){
-          add_theme_support('title-tag'); 
-
-          //These Section For Site Logo
-          add_theme_support('custom-logo', [
+        //These Section For Site Logo
+        add_theme_support('custom-logo', [
             'header-text' => ['site-title', 'site-description'],
-            'height'=> 100,
-		    'width' => 400,
-		    'flex-height' => true,
-		    'flex-width'  => true,
-	        'unlink-homepage-logo' => true, 
-        ]); 
-        }
+            'height' => 100,
+            'width' => 400,
+            'flex-height' => true,
+            'flex-width'  => true,
+
+        ]);
+
+        // It Is Use For Theme Default BackGround Color
+        add_theme_support(
+            'custom-background',
+            [
+                'default-color' => '#fff',
+                'default-image' => '',
+                'default-repeat' => 'no-repeat',
+            ]
+        );
+
+        add_theme_support( 'post-thumbnails' );
+
+        add_theme_support('customize-selective-refresh-widgets');
+
+        add_theme_support( 'automatic-feed-links');
+
+        add_theme_support(
+            'html5',
+            [
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'script',
+                'style',
+            ]
+            );
+
+           add_editor_style();
+           
+        //    add_theme_support();
+    }
 }
